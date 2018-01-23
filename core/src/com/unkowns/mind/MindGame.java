59e25849430f1a12ca7556a30a59af8959cfc034
thvardhan <thvardhan.god@gmail.com>
2018-01-23 20:21:51 +0530

    asset

--- core/src/com/unkowns/mind/MindGame.java
+++ core/src/com/unkowns/mind/MindGame.java
@@ -3,6 +3,7 @@ package com.unkowns.mind;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
@@ -11,6 +12,8 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.unkowns.mind.Screens.LogoScreen;
 import com.unkowns.mind.Screens.MainGameScreen;
+import com.unkowns.mind.managers.Text;
+import com.unkowns.mind.managers.TextLoader;
 
 public class MindGame extends Game {
 	public SpriteBatch batch;
@@ -29,7 +32,16 @@ public class MindGame extends Game {
 		camera = new OrthographicCamera();
         assetManager = new AssetManager();
         camera.setToOrtho(false, 1366, 768);
+
+        assetManager.setLoader(
+                Text.class,
+                new TextLoader(
+                        new InternalFileHandleResolver()
+                )
+        );
+        System.out.println("assetManager = " + assetManager.get("data/questions.txt").toString());
         loadAssets();
+
 
         if (!MindGame.DEBUG)
             this.setScreen(new LogoScreen(this));
@@ -45,6 +57,7 @@ public class MindGame extends Game {
             assetManager.load("texture/white.png", Texture.class);
             assetManager.load("texture/black.png", Texture.class);
             assetManager.load("texture/rectangleAlpha.png", Texture.class);
+            assetManager.load("data/questions.txt", Text.class);
             assetManager.finishLoading();
         } catch (Exception e) {
             e.printStackTrace();
